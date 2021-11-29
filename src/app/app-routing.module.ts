6d3aeb5ad8262874b42604de28cd34e41552dc30
Geovanny Reyes <geovanny.reyes@correounivalle.edu.co>
2021-11-29 10:12:02 -0500

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { SignupComponent } from './components/signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', component: AppComponent },
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: 'profile', component: ProfileComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
